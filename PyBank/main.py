abeea58a20e0045c7315f9b019786839334e26aa
Candida <candi.miranda@hotmail.com>
2023-01-08 21:59:36 -0600

    modified:   PyBank/main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,7 +3,7 @@ import os #module to create file path across operating systems
 import csv #module for reading and writing files
 
 #path to the csv file
-csvpath = os.path.join("..", "Resources", "budget_data.csv")
+csvpath = os.path.join("Resources", "budget_data.csv")
 
 #creating the variables
 months_count = 0
@@ -85,7 +85,7 @@ print(f"\nGreatest Increase in Profits: {increase_date} (${(increase_profit)})")
 print(f"\nGreatest Decrease in Profits: {decrease_date} (${(decrease_profit)})\n")
 
 #creating a text file in write mode
-with open("..", "analysis", "new.txt", 'w') as f:
+with open("analysis/new.txt", 'w') as f:
         
     #---------------------------------------------------
     #EXPORTING THE RESULTS IN A NEW TEXT FILE 
